a5951d14c80cff66ce3f2c885213876dcd2d2905
ParkJinMyung <wlsaud1992@naver.com>
2019-11-26 08:16:05 +0900

    로그인 메인 화면 수정 ing

--- twentyOnce/src/main/java/twentyOnce/sto/controller/StoController.java
+++ twentyOnce/src/main/java/twentyOnce/sto/controller/StoController.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.parsing.GenericTokenParser;
 import org.apache.ibatis.session.SqlSession;
-import org.junit.runner.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
